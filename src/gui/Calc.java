6816f405c64fb53f8aab2d533a26992daf817c6d
toomanybees <excessiveamountofbees@gmail.com>
2021-11-22 19:01:07 +1300

    add action listeners to 1234567890.+-*/ and DEL

--- src/gui/Calc.java
+++ src/gui/Calc.java
@@ -64,19 +64,38 @@ public class Calc {
 		btn_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				working_equation += btn_7.getText();
-				System.out.println(working_equation);
 				screen.setText(working_equation);
 			}
 		});
 		
 		JButton btn_8 = new JButton("8");
 		panel_buttons.add(btn_8);
+		btn_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_8.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_9 = new JButton("9");
 		panel_buttons.add(btn_9);
+		btn_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_9.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_DEL = new JButton("DEL");
 		panel_buttons.add(btn_DEL);
+		btn_DEL.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// remove the last char from working_equation
+				// https://www.javatpoint.com/how-to-remove-last-character-from-string-in-java
+				screen.setText(working_equation.substring(0, working_equation.length()-1));
+				working_equation = screen.getText();
+			}
+		});
 		btn_DEL.setBackground(Color.PINK);
 		btn_DEL.setToolTipText("Backspace");
 		
@@ -87,39 +106,111 @@ public class Calc {
 		
 		JButton btn_4 = new JButton("4");
 		panel_buttons.add(btn_4);
+		btn_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_4.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_5 = new JButton("5");
 		panel_buttons.add(btn_5);
+		btn_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_5.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_6 = new JButton("6");
 		panel_buttons.add(btn_6);
+		btn_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_6.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_multiply = new JButton("*");
 		panel_buttons.add(btn_multiply);
+		btn_multiply.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_multiply.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
-		JButton btn_divide = new JButton("\u00F7");
+		JButton btn_divide = new JButton("/");
 		panel_buttons.add(btn_divide);
+		btn_divide.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_divide.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_1 = new JButton("1");
 		panel_buttons.add(btn_1);
+		btn_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_1.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_2 = new JButton("2");
 		panel_buttons.add(btn_2);
+		btn_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_2.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_3 = new JButton("3");
 		panel_buttons.add(btn_3);
+		btn_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_3.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
-		JButton btn_add = new JButton("+");
-		panel_buttons.add(btn_add);
+		JButton btn_plus = new JButton("+");
+		panel_buttons.add(btn_plus);
+		btn_plus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_plus.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
-		JButton btn_subtract = new JButton("-");
-		panel_buttons.add(btn_subtract);
+		JButton btn_minus = new JButton("-");
+		panel_buttons.add(btn_minus);
+		btn_minus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_minus.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_0 = new JButton("0");
 		panel_buttons.add(btn_0);
+		btn_0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_0.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btn_decimal = new JButton(".");
 		panel_buttons.add(btn_decimal);
+		btn_decimal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				working_equation += btn_decimal.getText();
+				screen.setText(working_equation);
+			}
+		});
 		
 		JButton btnNewButton_7 = new JButton("New button");
 		panel_buttons.add(btnNewButton_7);
@@ -130,6 +221,13 @@ public class Calc {
 		JButton btn_equals = new JButton("=");
 		panel_buttons.add(btn_equals);
 		btn_equals.setBackground(new Color(153, 204, 255));
+		btn_equals.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+				//working_equation = (int) working_equation;
+				//screen.setText(working_equation);			//cast to a double
+			}
+		});
 	}
 
 }
