0b9a313e4f71e1427b8941fd30d530ab7118ff3d
toomanybees <excessiveamountofbees@gmail.com>
2022-01-03 01:32:26 +1300

    more rounding tests, tidy (2)

--- src/test/CalcWindowTest.java
+++ src/test/CalcWindowTest.java
@@ -41,14 +41,15 @@ class CalcWindowTest {
 	
 	
 	
-	
+	// (https://www.my-gcsescience.com/decimal-places-significant-figures/)
+	// (https://www.omnicalculator.com/math/sig-fig)
+	// (https://en.wikipedia.org/wiki/Significant_figures#Rules_to_identify_significant_figures_in_a_number)
 	@Test
 	public void testRoundingMethodSignificantFigures1() throws Exception
 	{
 		System.out.println("======== SF ========");
 		CalcWindow.setDecimalPrecision(1);
 		CalcWindow.setRoundingMethod("S");
-		//System.out.println(window.evaluateExpression("10.456"));
 		assertEquals("10", window.evaluateExpression("10.456"));
 	}
 	
@@ -57,7 +58,6 @@ class CalcWindowTest {
 	{
 		CalcWindow.setDecimalPrecision(2);
 		CalcWindow.setRoundingMethod("S");
-		//System.out.println(window.evaluateExpression("10.456"));
 		assertEquals("10", window.evaluateExpression("10.456"));
 	}
 	
@@ -66,7 +66,6 @@ class CalcWindowTest {
 	{
 		CalcWindow.setDecimalPrecision(3);
 		CalcWindow.setRoundingMethod("S");
-		//System.out.println(window.evaluateExpression("10.456"));
 		assertEquals("10.5", window.evaluateExpression("10.456"));
 	}
 	
@@ -75,7 +74,6 @@ class CalcWindowTest {
 	{
 		CalcWindow.setDecimalPrecision(4);
 		CalcWindow.setRoundingMethod("S");
-		//System.out.println(window.evaluateExpression("10.456"));
 		assertEquals("10.46", window.evaluateExpression("10.456"));
 	}
 	
@@ -84,8 +82,86 @@ class CalcWindowTest {
 	{
 		CalcWindow.setDecimalPrecision(5);
 		CalcWindow.setRoundingMethod("S");
-		//System.out.println(window.evaluateExpression("10.456"));
 		assertEquals("10.456", window.evaluateExpression("10.456"));
+	}
+	
+	
+	//rule: all non-zero digits are significant
+	@Test
+	public void testRoundingMethodSignificantFigures6() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(3);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("0.456", window.evaluateExpression("0.456"));
+	}
+	
+	//rule: zeroes placed between non-zero digits are significant
+	@Test
+	public void testRoundingMethodSignificantFigures7() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(5);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("5.0505", window.evaluateExpression("5.0505"));
+	}
+	
+	@Test
+	public void testRoundingMethodSignificantFigures8() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(3);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("5.05", window.evaluateExpression("5.0505"));
+	}
+	
+	@Test
+	public void testRoundingMethodSignificantFigures9() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(2);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("5.0", window.evaluateExpression("5.0404")); //round down
+	}
+	
+	
+	@Test
+	public void testRoundingMethodSignificantFigures10() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(2);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("5.1", window.evaluateExpression("5.0505")); //round UP
+	}
+	
+	//rule: leading zeroes are not significant
+	@Test
+	public void testRoundingMethodSignificantFigures11() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(3);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("0.456", window.evaluateExpression("0.456"));
+	}
+	
+	@Test
+	public void testRoundingMethodSignificantFigures12() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(3);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("0.000456", window.evaluateExpression("0.000456"));
+	}
+	
+	//rule: in an integer, trailing zeroes are NOT significant
+	@Test
+	public void testRoundingMethodSignificantFigures13() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(6);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("555000", window.evaluateExpression("555000"));
+	}
+	
+	//rule: in a decimal number, trailing zeroes ARE significant
+	@Test
+	public void testRoundingMethodSignificantFigures14() throws Exception
+	{
+		CalcWindow.setDecimalPrecision(4);
+		CalcWindow.setRoundingMethod("S");
+		assertEquals("5.555", window.evaluateExpression("5.555000"));
 	}
 	
 	
